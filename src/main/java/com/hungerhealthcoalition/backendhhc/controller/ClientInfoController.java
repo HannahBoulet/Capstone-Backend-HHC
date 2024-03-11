fbf33c0bf7eceedaec3eacf225d1b7baa71f35bb
HannahBoulet <Hannah.j.boulet@gmail.com>
2024-03-11 10:00:02 -0400

    base for methods

--- src/main/java/com/hungerhealthcoalition/backendhhc/controller/ClientInfoController.java
+++ src/main/java/com/hungerhealthcoalition/backendhhc/controller/ClientInfoController.java
@@ -1,9 +1,13 @@
 package com.hungerhealthcoalition.backendhhc.controller;
 
 
+import com.hungerhealthcoalition.backendhhc.model.ClientInfo;
 import com.hungerhealthcoalition.backendhhc.repository.ClientInfoRepository;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/Info")
@@ -14,6 +18,32 @@ public class ClientInfoController {
     {
         this.clientInfoRepository = clientInfoRepository;
     }
+
+    @GetMapping
+    public List<ClientInfo> getAllClientsInfos(){
+        return clientInfoRepository.findAll();
+    }
+
+    /**
+     * Methods needed:
+     * get specific client
+     * add client
+     * delete client
+     * update client
+     *
+     * */
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
