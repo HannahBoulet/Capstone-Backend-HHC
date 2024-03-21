c4eca2cb84ef23413c5ac20c58e73f80a2a1874d
NevanWijaya <wijayajn@appstate.edu>
2024-03-21 14:59:57 -0400

    fixed some pathing issues in Events Controller

--- src/main/java/com/hungerhealthcoalition/backendhhc/controller/EventsController.java
+++ src/main/java/com/hungerhealthcoalition/backendhhc/controller/EventsController.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/Logins")
+@RequestMapping("/api/Events")
 public class EventsController {
     private EventsRepository eventsRepository;
 
