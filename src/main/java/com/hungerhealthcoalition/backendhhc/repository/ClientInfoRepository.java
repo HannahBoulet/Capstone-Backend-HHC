fcd410dae41b42d9f62afcea915fd0796d9a5688
HannahBoulet <Hannah.j.boulet@gmail.com>
2024-03-21 14:20:53 -0400

    branch was pulling from issue 15 and not main.

--- src/main/java/com/hungerhealthcoalition/backendhhc/repository/ClientInfoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hungerhealthcoalition.backendhhc.repository;
-
-import com.hungerhealthcoalition.backendhhc.model.ClientInfo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ClientInfoRepository extends JpaRepository<ClientInfo, String> {
-}
